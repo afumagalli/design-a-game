009057b0ceaa2ec5f23b2e1246bb36f0eae5aefb
afumagalli <anthony.fumagalli@gmail.com>
2016-07-27 22:15:42 -0700

    Added Game class to models

--- pokemon-hangman/models.py
+++ pokemon-hangman/models.py
@@ -6,21 +6,39 @@ from datetime import date
 from protorpc import messages
 from google.appengine.ext import ndb
 
+POKEMON_LIST = PokemonNames()
+
 class User(ndb.model):
-    """User profile model"""
-    name = ndb.StringProprery(required=True)
-    email = ndb.StringProprery()
+	"""User profile model"""
+	name = ndb.StringProprery(required=True)
+	email = ndb.StringProprery()
 
 class Game(ndb.Model):
-    """Game object"""
-    target = ndb.StringProprery(required=True)
-    attempts_allowed = ndb.IntegerProperty(required=True)
-    attempts_remaining = ndb.IntegerProperty(required=True, default=6)
-    game_over = ndb.BooleanProperty(required=True, default=False)
-    user = ndb.KeyProperty(required=True, kind="User")
+	"""Game object"""
+	target = ndb.StringProprery(required=True)
+	attempts_remaining = ndb.IntegerProperty(required=True, default=6)
+	game_over = ndb.BooleanProperty(required=True, default=False)
+	user = ndb.KeyProperty(required=True, kind="User")
 
-    @classmethod
-    def new_game(cls, user):
-        """Creates and returns a new game"""
-        game = Game(user=user,
-                    word=pokemonNames().get_random_name())
+	@classmethod
+	def new_game(cls, user, number):
+		"""Creates and returns a new game"""
+		if number:
+			word = POKEMON_LIST.get_name(number)
+		else:
+			word = POKEMON_LIST.get_random_name()
+		game = Game(target=word,
+					attempts_remaining=6,
+					game_over=False
+					user=user)
+		game.put()
+		return game
+
+	def to_form(self, message):
+		"""Returns a GameForm representation of the Game"""
+		form = GameForm()
+
+	def end_game(self, won=False):
+		self.game_over = True
+		self.put()
+
